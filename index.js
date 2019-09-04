44959ce7601ed0c528cbc56d8d64475000d14895
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-04 19:41:00 +0800

    added Comments

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ http.createServer(function (req, res) {
   var filename = '';
 
   res.writeHead(200, {
-    'Context-Type': 'text/plain',
-    'Access-Control-Allow-Origin': '*'
+    'Context-Type': 'text/plain'
   });
-
+// whenever the server receives data from request // it reads the data  JSON.parse convert the string data to JSON or object
   req.on('data', function (data) {
     data = JSON.parse(data);
     body = data.name;
     filename = data.subject+'.txt';
   });
-
-  req.on('end', function (err) {
+// When the data is read the server create a file using the fs module
+  req.on('end', function () {
     fs.appendFile("subjects/"+filename, body+"\n");
   })
 
