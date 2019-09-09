c769fc991d9184d01c77441354de20e946ebe21a
Yol-B <54098157+Yol-B@users.noreply.github.com>
2019-09-09 09:39:52 +0800

    fixed Error

--- myModules/viewClass.js
+++ myModules/viewClass.js
@@ -1,21 +1,13 @@
-const fs = require('fs');
-
+var fs = require('fs');
 exports.viewClassList = function (res, string) {
     fs.readFile(string, function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         fs.writeFile('./Class/classList.csv', data, function (err) {
             if (err) throw err;
         });
-
     });
-<<<<<<< HEAD:myModules/viewClass.js
-    fs.readFile('./classList.html', function (err, data1) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data1);
-=======
     fs.readFile('classList.html', function (err, htmlData) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(htmlData);
->>>>>>> origin:viewClass.js
     });
 }
